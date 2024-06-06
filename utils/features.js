6d96e4265608c200307278dd51719b4efa1fac4f
Harsh <harshpachani@gmail.com>
2024-06-06 17:34:48 +0530

    Handle Cookies

--- utils/features.js
+++ utils/features.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import jwt from 'jsonwebtoken';
 
 const connectDb = () => {
     mongoose.connect(process.env.MONGO_URI, {
@@ -10,6 +11,27 @@ const connectDb = () => {
     .catch(err => { throw err });
 }
 
+const cookieOptions = {
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    sameSite: "none",
+    httpOnly: true,
+    secure: true,
+};
+
+const sendToken = (res, user, code, message) => {
+    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
+
+    return res
+            .status(code)
+            .cookie('token', token, cookieOptions).json({
+                success: true,
+                user,
+                message,
+            })
+}
+
 export {
     connectDb,
+    sendToken,
+    cookieOptions,
 }
