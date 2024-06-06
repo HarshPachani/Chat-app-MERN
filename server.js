a1ba9a7bb2fb2bf593f0d5ceb6d577aba013cfce
Harsh <harshpachani@gmail.com>
2024-06-06 17:35:41 +0530

    Added routes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDb } from './utils/features.js';
+import { errorMiddleware } from './middlewares/error.js'
+import cookieParser from 'cookie-parser';
+
+import userRoute from './routes/user.js';
+import chatRoute from './routes/chat.js';
 
 dotenv.config({
     path: './.env'
@@ -10,7 +15,13 @@ connectDb()
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(cookieParser());
 
+app.use('/api/v1/user', userRoute)
+app.use('/api/v1/chat', chatRoute)
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
