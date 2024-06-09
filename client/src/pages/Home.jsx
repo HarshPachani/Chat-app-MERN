3a85b60ceacc75857a514f06f7847a53d2d1d59e
Harsh <harshpachani@gmail.com>
2024-06-09 11:39:38 +0530

    Managed home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,22 @@ import { Box, Typography } from '@mui/material';
 import AppLayout from '../components/AppLayout'
 import { gray } from '../constants/color';
 import ChatList from '../components/ChatList';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+  const { user } = useSelector((store) => store.auth);
+
   return (
     <Box height='100%' bgcolor={gray} sx={{
-      display: { sm: 'flex', xs: 'none' }
+      display: { sm: 'flex', xs: 'none' },
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center'
     }}>
+      <Typography variant='h4' textAlign={'center'}>
+        Welcome { user.username }
+      </Typography>
+
       <Typography p='2rem' variant='h5' textAlign={'center'}>
         Select a friend to Chat
       </Typography>
