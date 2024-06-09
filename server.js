ef96ded300d855355102110889521e5cf0f7fa1c
Harsh <harshpachani@gmail.com>
2024-06-09 20:21:19 +0530

    Added START, STOP TYPING and NEW MESSAGE ALERT events

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ import dotenv from 'dotenv';
 import { connectDb } from './utils/features.js';
 import { errorMiddleware } from './middlewares/error.js'
 import cookieParser from 'cookie-parser';
-
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 import userRoute from './routes/user.js';
 import chatRoute from './routes/chat.js';
+import cors from 'cors';
+import corsOptions from './constants/config.js';
+import { socketAuthenticator } from './middlewares/auth.js';
+import { NEW_MESSAGE, NEW_MESSAGE_ALERT, START_TYPING, STOP_TYPING } from './constants/events.js';
+import { v4 as uuid } from 'uuid';
+import { getSockets } from './lib/helper.js';
+import { Message } from './models/message.js';
 
 dotenv.config({
     path: './.env'
@@ -13,16 +21,88 @@ dotenv.config({
 connectDb()
 
 const app = express();
+
+const server = createServer(app);
+const io = new Server(server, { cors: corsOptions });
+const userSocketIDs = new Map();
+
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 app.use('/api/v1/user', userRoute)
 app.use('/api/v1/chat', chatRoute)
 
 app.use(errorMiddleware);
 
-app.listen(PORT, () => {
+io.use((socket, next) => {
+    cookieParser()(
+        socket.request, 
+        socket.request.response, 
+        async(err) => await socketAuthenticator(err, socket, next)
+    );
+});
+
+io.on('connection', (socket) => {
+    const user = socket.user;
+    userSocketIDs.set(user._id.toString(), socket.id);
+    // console.log('Available Clients: ', userSocketIDs);
+    
+    socket.on(NEW_MESSAGE, async({ chatId, members, message }) => {
+        const messageForRealTime = {
+            _id: uuid(),
+            content: message,
+            sender: {
+                _id: user._id,
+                name: user.name,
+            },
+            chat: chatId,
+            createdAt: new Date().toISOString()
+        }
+
+        const messageForDB = {
+            content: message,
+            sender: user._id,
+            chat: chatId,
+        };
+
+        const memberSockets = getSockets(members);
+        io.to(memberSockets).emit(NEW_MESSAGE, {
+            chatId,
+            message: messageForRealTime,
+        });
+
+        io.to(memberSockets).emit(NEW_MESSAGE_ALERT, { chatId });
+
+        try {
+            await Message.create(messageForDB);
+        } catch (error) {
+            console.log(error.message);
+        }
+    });
+
+    socket.on(START_TYPING, ({ members, chatId }) => {
+        const membersSocket = getSockets(members);
+        socket.to(membersSocket).emit(START_TYPING, { chatId });
+    });
+    
+    
+    socket.on(STOP_TYPING, ({ members, chatId }) => {
+        const membersSocket = getSockets(members);
+        socket.to(membersSocket).emit(STOP_TYPING, { chatId });
+    });
+
+    socket.on('disconnect', () => {
+        userSocketIDs.delete(user._id.toString());
+        // onlineUsers.delete(user._id.toString());
+        // socket.broadcast.emit(ONLINE_USERS, Array.from(onlineUsers));
+    });
+});
+
+server.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
 })
+
+export { userSocketIDs }
