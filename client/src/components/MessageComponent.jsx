b8094fbf4b2b0e20769e439e64c8cc650e595d70
Harsh <harshpachani@gmail.com>
2024-06-12 18:30:23 +0530

    Added Animation

--- client/src/components/MessageComponent.jsx
+++ client/src/components/MessageComponent.jsx
@@ -1,24 +1,27 @@
 import { Box, Typography } from "@mui/material";
 import React, { memo } from "react";
-import { orange } from "../constants/color";
 import moment from "moment";
 import { fileFormat } from "../lib/features";
 import RenderAttachment from "../shared/RenderAttachment";
-// import { motion } from "framer-motion";
+import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
 
 const MessageComponent = ({ message, user }) => {
   const { sender, content, attachments = [], createdAt } = message;
   const sameSender = sender?._id === user?._id;
 
-  const timeAgo = moment(createdAt).fromNow();
+  const { theme } = useSelector(store => store.chat);
+//   const timeAgo = moment(createdAt).fromNow();
+  const timeAgo = moment(createdAt).format('h:mm A YYYY');
 
   return (
     // For Animation
-    // <motion.div
     <>
-    <div
+    {/* <div */}
+    <motion.div
       initial = {{ opacity: 0, x: '-100%' }}
       whileInView={{ opacity: 1, x: 0 }}
+
       style={{
         alignSelf: sameSender ? "flex-end" : "flex-start",
         width: "fit-content",
@@ -26,18 +29,15 @@ const MessageComponent = ({ message, user }) => {
     >
         <div
             style={{
-                backgroundColor: sameSender && attachments.length === 0 ? orange : 'rgb(238, 238, 238)',
+                // backgroundColor: sameSender && attachments.length === 0 ? orange : 'rgb(238, 238, 238)',
+                backgroundColor: sameSender && attachments.length === 0 ? theme : 'rgb(238, 238, 238)',
+                // backgroundColor: sameSender && attachments.length === 0 ? orange : lightBlue,
                 borderRadius: sameSender ? '15px 15px 0px 15px' : '15px 15px 15px 0px',
                 padding: "0.5rem",
                 color: sameSender && attachments.length === 0 ? 'white' : 'black',
                 overflow: 'auto'
             }}
         >
-            {!sameSender && (
-                <Typography color={orange} fontWeight={"600"} variant="caption">
-                {sender.name}
-                </Typography>
-            )}
             {content && <Typography>{content}</Typography>}
             {/* Attachment */}
             {attachments.length > 0 &&
@@ -64,10 +64,14 @@ const MessageComponent = ({ message, user }) => {
         <Typography
             variant="caption"
             color="text.secondary"
+            style = {{
+                alignSelf: sameSender ? 'flex-end' : '',
+                width: 'fit-content'
+            }}
         >
             {timeAgo}
         </Typography>
-    </div>
+    </motion.div>
     </>
   );
 };
