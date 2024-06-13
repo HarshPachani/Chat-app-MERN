89d7fb498208d0d14b8310c1949829b534c740b8
Harsh <harshpachani@gmail.com>
2024-06-13 17:27:30 +0530

    Manage color theme and animation

--- client/src/shared/ChatItem.jsx
+++ client/src/shared/ChatItem.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { Link } from '../styles/StyledComponents'
 import AvatarCard from './AvatarCard'
-import { Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import { gray, orange, white } from '../constants/color';
+import { useSelector } from 'react-redux';
+import { motion } from 'framer-motion';
 
 const ChatItem = ({
     avatar=[],
@@ -11,7 +13,11 @@ const ChatItem = ({
     groupChat=false,
     sameSender,
     isOnline,
+    index = 0,
+    newMessageAlert,
 }) => {
+  const { theme } = useSelector(store => store.chat);
+  
   return (
     <Link
       sx = {{
@@ -20,18 +26,47 @@ const ChatItem = ({
       }}
       to={`/chat/${_id}`}
     >
+        {/* <motion.div */}
         <div
-            style={{
-               display: "flex",
-               gap: "1rem",
-               alignItems: "center",
-               backgroundColor: sameSender ? gray : "unset",
-               color: sameSender ? "black" : "unset",
-               position: "relative",
-               padding: "1rem",
-            }}
+          // initial={{ opacity: 0, y:'-100%' }}
+          // whileInView={{ opacity: 1, y: 0 }}
+          // transition={{ delay: index * 0.01 }}
+        
+          style={{
+             display: "flex",
+             gap: "1rem",
+             alignItems: "center",
+             backgroundColor: sameSender ? gray : "unset",
+             color: sameSender ? "black" : "unset",
+            //  backgroundColor: sameSender ? gray : "black",
+            //  color: sameSender ? 'black' : 'white',
+             position: "relative",
+             padding: "1rem",
+          }}
         >
+          <div
+            style={{
+              position: 'relative',
+            }}
+          >
             <AvatarCard avatar={avatar}/>
+            {(isOnline && !groupChat) && (
+              <Box
+                sx={{
+                  width: '10px',
+                  height: '10px',
+                  borderRadius: '50%',
+                  backgroundColor: 'green',
+                  margin: '2px',
+                  marginBottom: 0,
+                  position: 'absolute',
+                  bottom: 0,
+                  right: '1rem',
+                  transform: 'translateY(-50%)',
+                }}
+              />
+            )}
+          </div>
             <Stack>
                 <Typography variant='h6'>{name}</Typography>
             </Stack>
@@ -40,15 +75,15 @@ const ChatItem = ({
                   fontSize: '0.75rem',
                   padding: '0px 7px',
                   margin: '3px',
-                  bgcolor: orange,
+                  bgcolor: theme,
                   color: white,
                   borderRadius: '50%',
                   position: 'absolute',
                   right: 0,
-              }}>1</Typography>
+              }}>{newMessageAlert?.count}</Typography>
         </div>
     </Link>
   )
 }
 
-export default ChatItem
+export default memo(ChatItem)
