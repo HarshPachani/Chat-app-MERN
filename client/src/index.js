24347f9cfc99d7354b523e702361df5ca42b15eb
Harsh <harshpachani@gmail.com>
2024-06-13 17:29:01 +0530

    Added Redux store provider

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async'
 import { CssBaseline } from '@mui/material';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <CssBaseline />
-      <App />
-    </HelmetProvider>
+    <Provider store={store}>
+      <HelmetProvider>
+        <CssBaseline />
+        <App />
+      </HelmetProvider>
+    </Provider>
   </React.StrictMode>
 );
 
