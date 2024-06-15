58b29538a64b51881da5d65e5b8f5dee0261e504
Harsh <harshpachani@gmail.com>
2024-06-15 21:43:38 +0530

    Manage socket events

--- client/src/pages/Group.jsx
+++ client/src/pages/Group.jsx
@@ -1,16 +1,19 @@
-import React, { Suspense, lazy, useCallback, useEffect, useState } from 'react'
-import ManageGroupLayout, { GroupsList } from '../components/ManageGroupLayout';
+import React, { Suspense, lazy, memo, useCallback, useEffect, useState } from 'react'
+import ManageGroupLayout from '../components/ManageGroupLayout';
 import { Backdrop, Box, Button, CircularProgress, Grid, IconButton, Menu as MenuIcon, Stack, TextField, Tooltip, Typography } from '@mui/material';
-import { gray, orange } from '../constants/color';
+import { gray, orange, white } from '../constants/color';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { Add as AddIcon, Delete as DeleteIcon, Done as DoneIcon, Edit as EditIcon, KeyboardBackspace as KeyboardBackspaceIcon } from '@mui/icons-material';
+import { Add as AddIcon, Delete as DeleteIcon, Done as DoneIcon, Edit as EditIcon, KeyboardBackspace as KeyboardBackspaceIcon, AccountCircle as ProfileIcon } from '@mui/icons-material';
 import { useDeleteChatMutation, useGetChatDetailsQuery, useRemoveGroupMemberMutation, useRenameGroupMutation } from '../redux/api/api';
 import UserItem from '../shared/UserItem';
 import { useAsyncMutation, useErrors, useSocketEvents } from '../hooks/Hook';
 import { useDispatch, useSelector } from 'react-redux';
-import { setIsAddMember } from '../redux/reducers/misc';
+import { setIsAddMember, setIsProfile } from '../redux/reducers/misc';
 import { useSocket } from '../context/socket';
-import { REFETCH_CHATS } from '../constants/events';
+import { REFETCH_CHATS, REFETCH_GROUP_CHAT_MEMBERS } from '../constants/events';
+import IconBtn from '../components/IconButton'
+import { InputBox, Link } from '../styles/StyledComponents';
+import AvatarCard from '../shared/AvatarCard';
 
 const ConfirmDeleteDialog = lazy(() => import('../dialogs/ConfirmDeleteDialog'));
 const AddMemberDialog = lazy(() => import('../dialogs/AddMemberDialog'));
@@ -18,6 +21,7 @@ const AddMemberDialog = lazy(() => import('../dialogs/AddMemberDialog'));
 const Group = ({ myGroups }) => {
   
   const chatId = useSearchParams()[0].get("group");
+  
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const socket = useSocket();
@@ -29,15 +33,17 @@ const Group = ({ myGroups }) => {
     { skip: !chatId }
   );
 
-  // const refetchListener = useCallback((data) => {
-  //   refetch();
-  // }, [refetch]);
 
-  // const socketHandlers = {
-  //   [REFETCH_CHATS]: refetchListener,
-  // }
+  const refetchListener = useCallback((data) => {
+    console.log("Refetching...");
+    refetch();
+  }, [refetch]);
 
-  // useSocketEvents(socket, socketHandlers);
+  const socketHandlers = {
+    [REFETCH_GROUP_CHAT_MEMBERS]: refetchListener,
+  }
+
+  useSocketEvents(socket, socketHandlers);
 
   const [groupName, setGroupName] = useState('');
   const [groupNameUpdatedValue, setGroupNameUpdatedValue] = useState('');
@@ -80,6 +86,10 @@ const Group = ({ myGroups }) => {
   }, [data]);
 
   useEffect(() => {
+    setGroupName(groupNameUpdatedValue);
+  }, [groupNameUpdatedValue]);
+
+  useEffect(() => {
     if (chatId && data) {
       setGroupName(`${data?.chat?.name}`);
       setGroupNameUpdatedValue(`${data?.chat?.name}`);
@@ -91,7 +101,7 @@ const Group = ({ myGroups }) => {
       setIsEdit(false);
     };
   }, [chatId]);
-
+  
   const navigateBack = () => {
     navigate("/groups");
   };
@@ -121,7 +131,9 @@ const Group = ({ myGroups }) => {
   const removeMemberHandler = (userId) => {
     removeMember('Removing member...', { chatId, userId});
   };
+
   
+
   const IconBtns = (
     <>
       <Box
@@ -139,6 +151,7 @@ const Group = ({ myGroups }) => {
       <Tooltip title='back'>
         <IconButton
           sx={{
+            display: chatId ? 'block' : 'none',
             position: 'absolute',
             top: '2rem',
             left: '2rem',
@@ -187,7 +200,6 @@ const Group = ({ myGroups }) => {
     </Stack>
   );
   
-  
   const GroupName = (
     <>
       <Stack
@@ -226,20 +238,7 @@ const Group = ({ myGroups }) => {
   );
   
   return (
-    <Grid container height={'100vh'}>
-      <Grid
-        item
-        sx={{
-          display: {
-            xs: 'none',
-            sm: 'block',
-          },
-        }}
-        sm={4}
-      >
-        <GroupsList myGroups={myGroups} />
-      </Grid>
-      <Grid
+    <Grid
         item
         xs={12}
         sm={8}
@@ -273,14 +272,14 @@ const Group = ({ myGroups }) => {
             >
               {/* Members */}
               { isLoadingRemoveMember ? <CircularProgress /> :
-               members.map((i) => (
+               members?.map((i) => (
                 <UserItem
                   key={i._id}
                   user={i}
                   isAdded
                   styling={{
                     boxShadow: '0 0 0.5rem rgba(0, 0, 0, 0.2)',
-                    padding: '1rem 2rem',
+                    padding: { xs: 0, sm: '1rem 2rem' },
                     borderRadius: '1rem',
                   }}
                   handler={removeMemberHandler}
@@ -305,9 +304,130 @@ const Group = ({ myGroups }) => {
             />
           </Suspense>
         )}
-      </Grid>
     </Grid>
   )
 }
 
-export default ManageGroupLayout()(Group);
+const GroupsList = ({ w = "100%", myGroups = [], chatId }) => {
+  
+  const [search, setSearch] = useState('');
+  const [groupChatList, setGroupChatList] = useState(myGroups);
+  
+  const { user } = useSelector(store => store.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setGroupChatList(myGroups);
+}, [myGroups])
+
+  useEffect(() => {
+    if(search.trim() === '') {
+        setGroupChatList(myGroups);
+    }
+    
+    myGroups = myGroups?.filter(group => group?.name?.toLowerCase().includes(search?.toLowerCase().trim()));
+      setGroupChatList(myGroups);
+    }, [search]);
+
+    const openProfile = () => dispatch(setIsProfile(true));
+
+    const handleSearchChange = (e) => {
+      setSearch(e.target.value);
+    }
+
+    return (
+      <Grid
+        item
+        sx={{
+          display: { xs:  chatId ? 'none' : 'block', sm: 'block' },
+          marginBottom: '5px',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: { xs: 'column', md: 'row' },
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            backgroundColor: white,
+            borderRadius: '15px',
+            padding: '5px',
+            height: { xs: '80px', sm: '40px' },
+          }}
+          margin={'5px'}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: { xs: '100%', sm: 'auto' },
+            }}
+          >
+            <Typography variant='h5' sx={{ marginLeft: '5px' }}>Groups</Typography>
+            <IconBtn 
+              title={user?.username}
+              icon={<ProfileIcon />}
+              sx={{
+                display: { xs: 'flex', sm: 'none' }
+              }}
+              onClick={openProfile}
+            />
+          </Box>
+          <InputBox 
+            placeholder="Search Groups..."
+            value={search}
+            onChange={handleSearchChange}
+          />
+        </Box>
+        <Stack
+          width={w}
+          direction="column"
+          sx={{ 
+            height: '100%', 
+            overflow: 'auto', 
+            backgroundColor: white, 
+            borderRadius: '20px',
+            marginRight: '5px',
+            border: `2px solid ${white}`,
+          }}
+        >
+          {groupChatList?.length > 0 ? (
+            groupChatList.map((group) => (
+              <GroupListItem key={group._id} group={group} chatId={chatId} />
+            ))
+          ) : (
+            <Typography textAlign="center" padding="1rem">
+              No groups
+            </Typography>
+          )}
+        </Stack>
+      </Grid>
+    );
+};
+
+const GroupListItem = memo(({ group, chatId }) => {
+  const { name, avatar, _id } = group;
+  const currentChat = useSearchParams()[0].get("group");
+
+  return (
+    <Link
+      to={`?group=${_id}`}
+      onClick={(e) => {
+        if (chatId === _id) e.preventDefault();
+      }}
+      style={{
+        backgroundColor: currentChat === _id ? gray : '' 
+      }}
+    >
+      <Stack direction={"row"} spacing={"1rem"} alignItems={"center"}>
+        <AvatarCard avatar={avatar} />
+        <Typography>{name}</Typography>
+      </Stack>
+    </Link>
+  );
+});
+
+export default memo(ManageGroupLayout()(Group));
+export { GroupListItem, GroupsList };
