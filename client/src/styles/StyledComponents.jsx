b564ecbbce4962a5c2d37cef66c3bcc9c4eb94e0
Harsh <harshpachani@gmail.com>
2024-06-15 12:32:52 +0530

    Manage input styles

--- client/src/styles/StyledComponents.jsx
+++ client/src/styles/StyledComponents.jsx
@@ -31,4 +31,16 @@ const BouncingSkeleton = styled(Skeleton)(() => ({
     animation: `${bounceAnimation} 1s infinite`
 }))
 
-export { Link, InputBox, BouncingSkeleton };
+const VisuallyHiddenInput = styled('input')({
+    border: 0,
+    clip: 'react(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    whiteSpace: 'nowrap',
+    width: 1,
+});
+
+export { Link, InputBox, BouncingSkeleton, VisuallyHiddenInput };
