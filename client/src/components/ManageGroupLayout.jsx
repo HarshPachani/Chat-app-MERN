b49ad24bbbb8c3a535b1c99d6a07984253d2bcac
Harsh <harshpachani@gmail.com>
2024-06-21 18:54:26 +0530

    Manage Socket events

--- client/src/components/ManageGroupLayout.jsx
+++ client/src/components/ManageGroupLayout.jsx
@@ -5,70 +5,74 @@ import { gray } from '../constants/color'
 import { useMyGroupsQuery } from '../redux/api/api'
 import { useSearchParams } from 'react-router-dom'
 import { useSocketEvents } from '../hooks/Hook'
-import { REFETCH_CHATS } from '../constants/events'
+import { REFETCH_CHATS, NEW_MESSAGE_ALERT, NEW_REQUEST } from '../constants/events'
 import { useSocket } from '../context/socket'
 import { GroupsList } from '../pages/Group'
 import Title from '../shared/Title'
+import { useDispatch, useSelector } from 'react-redux'
+import { incrementNotification, setNewMessageCount, setNewMessagesAlert } from '../redux/reducers/chat'
+import { getOrSaveFromStorage } from '../lib/features'
 
 const manageGroupLayout = () => (WrappedComponent) => {
-    return (props) => {
-      const socket = useSocket();
-      const [groupName, setGroupName] = useState('');
-      const [groupNameUpdatedValue, setGroupNameUpdatedValue] = useState('');
-      const [isEdit, setIsEdit] = useState(false);
-      
-      const { data , refetch } = useMyGroupsQuery('');
-      
-      const refetchListener = useCallback((data) => {
-        refetch();
-      }, [refetch]);
+  return (props) => {
+    const socket = useSocket();
+    const dispatch = useDispatch();
+    
+    const { data , refetch } = useMyGroupsQuery('');
 
-      const eventHandlers = {
-        [REFETCH_CHATS]: refetchListener,
-      }
-      useSocketEvents(socket, eventHandlers);
+    const { newMessageAlert } = useSelector(store => store.chat);
+    
+    const refetchListener = useCallback((data) => {
+      refetch();
+    }, [refetch]);
 
-      const chatId = useSearchParams()[0].get('group');
+    const newRequestListener = useCallback(() => {
+        dispatch(incrementNotification());
+    }, [dispatch]);
 
-      useEffect(() => {
-        if(chatId) {
-          setGroupName(`Group Name ${chatId}`);
-          setGroupNameUpdatedValue(`Group Name ${chatId}`);
-        }
+    const newMessageAlertListener = useCallback((data) => {
+      dispatch(setNewMessagesAlert(data));
+    }, [dispatch]);
 
-        return () => {
-          setGroupName(``);
-          setGroupNameUpdatedValue(``);
-          setIsEdit(false);
-        };
-      }, [chatId]);
-
-      return (
-        <Box           
-          sx={{
-            display: 'flex',
-            // position: 'fixed',
-            position: {xs: 'relative', sm: 'fixed' },
-            flexDirection: { xs: 'column-reverse', sm: 'row'},
-            backgroundColor: gray,
-            // overflow: 'none',
-            height: '100%',
-            width: '100%'
-          }}
-        >
-          <Title />
-          <SideBar chatId={chatId} />
-          <GroupsList
-            myGroups={data?.groups}
-            chatId={chatId}
-          />
-          <WrappedComponent 
-            {...props}
-            myGroups={data?.groups}
-          />
-        </Box>
-      )
+    const eventHandlers = {
+      [REFETCH_CHATS]: refetchListener,
+      [NEW_MESSAGE_ALERT]: newMessageAlertListener,
+      [NEW_REQUEST]: newRequestListener,
     }
+    useSocketEvents(socket, eventHandlers);
+
+    const chatId = useSearchParams()[0].get('group');
+
+    
+    useEffect(() => {
+      getOrSaveFromStorage({ key: NEW_MESSAGE_ALERT, value: newMessageAlert });
+      dispatch(setNewMessageCount())
+    }, [newMessageAlert, dispatch]);
+
+    return (
+      <Box           
+        sx={{
+          display: 'flex',
+          position: {xs: 'relative', sm: 'fixed' },
+          flexDirection: { xs: 'column-reverse', sm: 'row'},
+          backgroundColor: gray,
+          height: '100%',
+          width: '100%'
+        }}
+      >
+        <Title />
+        <SideBar chatId={chatId} />
+        <GroupsList
+          myGroups={data?.groups}
+          chatId={chatId}
+        />
+        <WrappedComponent 
+          {...props}
+          myGroups={data?.groups}
+        />
+      </Box>
+    )
+  }
 }
 
 
